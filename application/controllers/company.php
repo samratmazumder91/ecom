f2a3067495d3996b975f0885db7b51b36f1e7901
Samrat <samratmazumder91@gmail.com>
2014-02-20 01:00:45 +0530

    new changes

---
+++ application/controllers/company.php
@@ -0,0 +1,11 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Company extends CI_Controller {
+	public function index()
+	{
+		$this->load->view('home');
+	}
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
